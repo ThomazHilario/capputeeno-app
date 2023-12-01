23fffab3ed743cf20d584305241dd116c6108014
Thomaz Alves <thomazhilario5@gmail.com>
2023-12-01 11:18:37 -0300

    Organizando as pasta dos componentes

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../Components/Home'
 import Produto from '../Components/Produto'
-import Carrinho from '../Components/carrinho'
+import Carrinho from '../Components/Carrinho'
 
 export default function RoutesNavegation(){
     return(
