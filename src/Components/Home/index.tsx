37a5353b269e49ea08636c314db939f095be6668
Thomaz Alves <thomazhilario5@gmail.com>
2023-12-04 15:57:21 -0300

    Step-button
    - Implementando o a logica das funções do step-button.

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -215,6 +215,8 @@ function NavegationProgress(props:NavegationProps){
         // Caso contador seja abaixo de 0
         if(cont < 0){
             setCont(0)
+            setPrev(0)
+            setNext(12)
         }
 
         // Caso contador seja maio que o total de buttons
@@ -222,34 +224,56 @@ function NavegationProgress(props:NavegationProps){
             setCont(document.querySelectorAll('.step-button').length)
         }
 
-
-    },[cont])
+    },[cont, setPrev, setNext])
 
     // Voltando na pagina
     function prevProgress(){
-        // Incrementa no contador
-        setCont(cont - 1)
+
+        // Armazenando o valor de const a uma variavel
+        const value = cont - 2
 
         // Captura os buttons por meio do querySelector
         const button = document.querySelectorAll<HTMLElement>('.step-button')
 
         // Percorrendo cada button
-        button.forEach((button) => {
+        button.forEach((button,idx) => {
             
-            
-            if(parseFloat(button.textContent as string) - 1 === cont){
-                button.style.backgroundColor = 'white'
+            // Caso o value seja igual ao idx do button
+            if(value === idx){
 
+                // Decremento o contador
+                setCont(cont - 1)
+
+                // Alterando o background do step-button
+                button.style.border = '1px solid orange'
+                button.style.backgroundColor = 'rgb(203 213 225)'
+
+                // Alterando o slice do array
                 setPrev(prev - 12)
                 setNext(next - 12)
+
+                
+            } else if(cont === 1){
+                // Decremento o contador
+                setCont(cont -2)
+
+                // Adicionando style ao step button
+                button.style.border = '0px'
+                button.style.backgroundColor = 'rgb(203 213 225)'
+
+            } else{
+
+                // Adicionando style ao step button
+                button.style.border = '0px'
+                button.style.backgroundColor = 'rgb(203 213 225)'
             }
         })
     }
 
     function nextProgress(){
-        // Incrementa no contador
-        setCont(cont + 1)
-
+        // 
+        const value = cont
+        
         // Captura os buttons
         const button:NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.step-button')
 
@@ -257,17 +281,30 @@ function NavegationProgress(props:NavegationProps){
         button.forEach((button,idx) => {
 
             // Caso o contador seja igual ao idx
-            if(cont === idx){
-                button.style.backgroundColor = 'blue'
+            if(value === idx){
+
+                // Incrementa no contador
+                setCont(cont + 1)
+                button.style.backgroundColor = 'rgb(203 213 225)'
+                button.style.border = '1px solid orange'
+
+                // Alterando oo slice do array
                 setPrev(prev + 12)
                 setNext(next + 12)
+                
+
+            } else if(cont < 4){
+
+                // Adicionando style ao step-button
+                button.style.border = '0px'
+                button.style.backgroundColor = 'rgb(203 213 225)'
             }
         })
     }
 
     return(
         <div className='flex gap-1 justify-end mt-4' id='navegatinProgress'>
-            <button className='step-button-only bg-blue-500' disabled>1</button>
+            <button id='step-button-only' className='bg-slate-300'>1</button>
             <button className='step-button'>2</button>
             <button className='step-button'>3</button>
             <button className='step-button'>4</button>
