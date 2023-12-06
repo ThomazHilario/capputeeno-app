92419b5275a3627ba742d61d2dfd1c958d297c41
Thomaz Alves <thomazhilario5@gmail.com>
2023-12-06 18:07:56 -0300

    Adicionando o context na aplicação toda

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Link } from 'react-router-dom'
+import ContextCart from './Context/context'
 import RoutesNavegation from './Routes/routes'
 import bagIcon from './assets/bag_icons/bag32.png'
 import './index.css'
@@ -31,31 +32,33 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <ContextCart>
+        <BrowserRouter>
 
-        {/* header */}
-        <header className='flex justify-between items-center pr-44 pl-44' id='cabecalho'>
+          {/* header */}
+          <header className='flex justify-between items-center pr-44 pl-44' id='cabecalho'>
 
-          {/* titulo logo */}
-          <Link to='/'><h1 className='titulo'>Capputeeno</h1></Link>
+            {/* titulo logo */}
+            <Link to='/'><h1 className='titulo'>Capputeeno</h1></Link>
 
-          {/* Nav */}
-          <nav className='flex gap-5'>
-            {/* formulario */}
-            <form>
-              <input type='text' placeholder='Procurando por algo em especifico?' onChange={(e) => seachProduct(e.target.value)} className='w-80 p-1 pl-4 bg-gray-100 rouded-sm'/>
-            </form>
+            {/* Nav */}
+            <nav className='flex gap-5'>
+              {/* formulario */}
+              <form>
+                <input type='text' placeholder='Procurando por algo em especifico?' onChange={(e) => seachProduct(e.target.value)} className='w-80 p-1 pl-4 bg-gray-100 rouded-sm'/>
+              </form>
 
-            {/* icone carrinho de compras */}
-            <div id='carrinho'>
-              <Link to='/carrinho'><img src={bagIcon} alt='imagem do carrinho' className='cursor-pointer'/></Link>
-            </div>
-          </nav>
-          
-        </header>
+              {/* icone carrinho de compras */}
+              <div id='carrinho'>
+                <Link to='/carrinho'><img src={bagIcon} alt='imagem do carrinho' className='cursor-pointer'/></Link>
+              </div>
+            </nav>
+            
+          </header>
 
-        <RoutesNavegation/>
-      </BrowserRouter>
+          <RoutesNavegation/>
+        </BrowserRouter>
+      </ContextCart>
     </>
   )
 }
