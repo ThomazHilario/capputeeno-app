dd98d97a31b8fd2d16eebceb604a34cdb007b580
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-03 15:04:01 -0300

    Refactor: ProdutoCard Component
    - Criando um componente para exibi os produtos, separado do componente Home.

---
+++ src/Components/produtoCard.tsx
@@ -0,0 +1,37 @@
+// imports React
+import { useNavigate } from 'react-router-dom'
+
+// imports interfaces
+import { ProdutoProps } from '../interfaces/homeTypes'
+
+export const Produto = ({img,name,price}:ProdutoProps) => {
+    // Navigate
+    const navigate = useNavigate()
+
+    // navegando ate a pagina
+    function navegationPage(){
+
+        localStorage.setItem('@item',JSON.stringify({
+            image_url:img,
+            name:name,
+            price_in_cents:price
+        }))
+
+        navigate(`/produto/${name}/${price}`)
+    }
+
+    return(
+        <div className='produto bg-gray-100 rounded-t-lg' onClick={navegationPage}>
+
+                {/* imagem do produto */}
+                <img src={img} alt='imagem do produto' className='h-auto rounded-t-lg'/>
+
+                {/* Descricao dos produtos */}
+                <div id='descricao-dos-produtos' className='flex flex-col gap-2 p-3 '>
+                    <p className='name_produto text-1xl'>{name}</p>
+                    <hr/>
+                    <p><strong>RS {Math.ceil(price / 80).toFixed(2).replace('.',',')}</strong></p>
+                </div>
+        </div>
+    )
+}
