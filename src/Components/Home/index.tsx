e1113c8f17f750288e54464c18b4337315e1f9e5
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-03 14:54:42 -0300

    Refactor: FilterProducts Compoent
    - Criando um componente separa do arquivo Home.

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,17 +1,18 @@
 import '../../index.css'
 
 // Import react
-import {useState, useEffect, useMemo } from 'react'
+import {useState, useEffect } from 'react'
 import {useNavigate} from 'react-router-dom'
 
 // Import context
 import { UseStatesProps } from '../../Context/context'
 
 // Import Interface
-import { Users, ProdutoProps, NavegationProps, FilterProduct } from '../../interfaces/homeTypes'
+import { Users, ProdutoProps, NavegationProps } from '../../interfaces/homeTypes'
 
 // import Components
 import { Button } from '../button'
+import { FilterProducts } from '../filterProducts'
 
 // Componente Home - Principal
 export default function Home(){
@@ -89,56 +90,6 @@ export default function Home(){
         )
    }
 }
-
-// Componente FilterProducts
-function FilterProducts({lista,setLista}:FilterProduct){
- 
-    // Salvando a lista no cache, evitando fazer outra requisição
-    const listaDefaultCache = useMemo(() => {
-        return lista
-    },[lista])
-
-    // state - listaDefault
-    const [listaDefault, setListaDefault] = useState([...listaDefaultCache])
-
-
-    // Filter
-    function filter(option:string){
-        // Setando a lista do cache
-        setListaDefault([...listaDefaultCache])
-
-        // Opções de filtragens
-        if(option === 'maior'){
-            const newLista = listaDefault.sort((a,b) => b.price_in_cents - a.price_in_cents)
-            setLista(newLista)
-        } 
-        
-        if(option === 'menor'){
-            const newLista = listaDefault.sort((a,b) => a.price_in_cents - b.price_in_cents)
-            setLista(newLista)
-        }
-        
-        if(option === 'maisVendidos'){
-            const newLista = listaDefault.sort((a,b) => a.sales - b.sales)
-            setLista(newLista)
-        }
-
-        if(option === 'default'){
-            setLista(listaDefault)
-        }
-
-    }
-
-    return(
-        <select name="filtro" id="filtro-de-selecao" className='w-40 mt-4 md:mt-0' onChange={(e) => filter(e.target.value)}>
-            <option value="default">Organizar por</option>
-            <option value="maior">Preço: maior-menor</option>
-            <option value="menor">Preço: menor-maior</option>
-            <option value="maisVendidos">Mais vendidos</option>
-        </select>
-    )
-}
-
 
 // Componente Produto
 function Produto({img,name,price}:ProdutoProps){
