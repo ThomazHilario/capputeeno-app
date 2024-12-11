506b68e26461c741838f1e87e979ea87811f6508
Thomaz Alves <thomazhilario5@gmail.com>
2024-12-11 11:14:22 -0300

    fix: Cancel refresh in page

--- src/Components/Header/Seach/search-input.tsx
+++ src/Components/Header/Seach/search-input.tsx
@@ -1,14 +1,21 @@
 // Context
+import { FormEvent } from "react"
 import { UseStatesProps } from "../../../Context/context"
 
 export const SeachInput = () => {
 
     // Context - states
-    const { setSeach } = UseStatesProps()
+    const { seach, setSeach } = UseStatesProps()
+
+    // Cancel refresh page
+    const cancelPageRefreshing = (e:FormEvent) => {
+        // Method cancel page refreshing
+        e.preventDefault()
+    }
 
     return(
-        <form className="min-w-[300px]">
-            <input type='text' placeholder='Procurando por algo em especifico?' onChange={(e) => setSeach(e.target.value)} className='w-full p-1 pl-4 bg-gray-100 rouded-sm'/>
+        <form className="min-w-[300px]" onSubmit={(e) => cancelPageRefreshing(e)}>
+            <input type='text' value={seach} placeholder='Procurando por algo em especifico?' onChange={(e) => setSeach(e.target.value)} className='w-full p-1 pl-4 bg-gray-100 rouded-sm'/>
         </form>
     )
 }
